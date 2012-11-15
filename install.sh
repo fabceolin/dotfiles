0f7c87e31af7edb2918f016aa7f3614803e814ca
Fabrício Ceolin <ceolin@doarse.com.br>
2012-11-15 11:15:55 -0200

    Insert vimrc file

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd ~
-ln -sf etc/.xmonad .xmonad
-ln -sf etc/bin bin
-ln -sf etc/.vim .vim
-# inserir o bin no path pelo .bashrc
+ln -sf dotfiles/.xmonad .xmonad
+ln -sf dotfiles/bin bin
+ln -sf dotfiles/.vim .vim
+ln -sf dotfiles/.vimrc .vimrc
+# TODO - Insert ~/bin/ on PATH at .bashrc
 cd -
 gconftool-2 --load gnome-terminal.gconftool
