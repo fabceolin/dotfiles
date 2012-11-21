535809ae45ec936b30ae3e441522aa40f98962cd
Fabricio Ceolin <ceolin@ulevel.com>
2012-11-22 00:26:17 +0400

    Installing tmux.conf

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf dotfiles/.xmonad .xmonad
 ln -sf dotfiles/bin bin
 ln -sf dotfiles/.vim .vim
 ln -sf dotfiles/.vimrc .vimrc
+ln -sf dotfiles/.tmux.conf .tmux.conf
 # TODO - Insert ~/bin/ on PATH at .bashrc
 cd -
 cd ~/.ssh
