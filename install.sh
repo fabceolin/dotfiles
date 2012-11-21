966cab67650e292ab8bff2758c53f788087ae0d8
Fabrício Ceolin <ceolin@ulevel.com>
2012-11-21 14:47:26 -0200

    Installing ssh config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf dotfiles/.xmonad .xmonad
 ln -sf dotfiles/bin bin
 ln -sf dotfiles/.vim .vim
 ln -sf dotfiles/.vimrc .vimrc
+ln -sf dotfiles/.ssh/config .ssh/config
 # TODO - Insert ~/bin/ on PATH at .bashrc
 cd -
 gconftool-2 --load gnome-terminal.gconftool
