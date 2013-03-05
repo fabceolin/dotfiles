add6ee38e387d3078639fb77867b8fa6538168f9
Fabrício Ceolin <fabricio.ceolin@miningmath.com>
2013-03-05 17:29:52 -0300

    Getting better script to manage pontodosconcursos pdf

--- bin/pontodosconcursos-anonimo.sh
+++ bin/pontodosconcursos-anonimo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 FILE="$1"
-convert "$FILE" -white-threshold 90% "$FILE"
-pdfcrop --margins "0 0 0 -30" "$FILE" "$FILE"
+convert "$FILE" -white-threshold 90% "${FILE%%.pdf}.mod1.pdf"
+pdfcrop --margins "0 0 0 -30" "${FILE%%.pdf}.mod1.pdf" "${FILE%%.pdf}.mod.pdf"
+rm "${FILE%%.pdf}.mod1.pdf"
