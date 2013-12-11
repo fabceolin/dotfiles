ef0b2aadefb7be0005937bcf80576fc962b53cd8
Fabrício Ceolin <fabricio.ceolin@miningmath.com>
2013-12-11 13:39:58 -0200

    Changing ffmpeg parameters for a better screencast

--- bin-win/ffmpeg-screencastInternalMicrophone.bat
+++ bin-win/ffmpeg-screencastInternalMicrophone.bat
@@ -1,1 +1,1 @@
- ffmpeg -f dshow -i audio="Microfone (USB Audio CODEC )":video="screen-capture-recorder" screencast-%RANDOM%-%RANDOM%.mkv
+ ffmpeg -f dshow -r 5 -i audio="Microfone (USB Audio CODEC )":video="screen-capture-recorder" screencast-%RANDOM%-%RANDOM%.webm
